0f5b702b8dfcfa9cfaf314a4b4aae9bb8507e5a8
Marcos Lautaro Molina <marcoslautaromolina@gmail.com>
2026-03-01 22:04:25 -0300

    borre todo lo que tenga q ver con Login por q merge la rama login a la rama main y no al dev

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -1,22 +1,12 @@
-import { useState } from "react";
 import { Outlet } from "react-router";
 import NavbarGym from "./NavbarGym";
 import FooterGym from "./FooterGym";
-import LoginModal from "./LoginModal";
 
 const Layout = () => {
-  const [showLogin, setShowLogin] = useState(false);
-
-  const handleShowLogin = () => setShowLogin(true);
-  const handleCloseLogin = () => setShowLogin(false);
-
   return (
     <div className="d-flex flex-column min-vh-100 bg-dark text-light">
       {/* Navbar fijo en la parte superior */}
-      <NavbarGym onLoginClick={handleShowLogin} />
-
-      {/* Modal de Login */}
-      <LoginModal show={showLogin} handleClose={handleCloseLogin} />
+      <NavbarGym />
 
       {/* Contenido dinámico de las páginas */}
       <main className="flex-grow-1">
@@ -30,3 +20,4 @@ const Layout = () => {
 };
 
 export default Layout;
+

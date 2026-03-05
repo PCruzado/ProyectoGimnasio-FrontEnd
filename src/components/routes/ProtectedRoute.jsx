677d32a350decd4016bdbcf3188ae5182b2b9824
Vera_Facundo <facundovera647@gmail.com>
2026-03-04 22:04:27 -0300

    corrijo el protector de rutas

--- src/components/routes/ProtectedRoute.jsx
+++ src/components/routes/ProtectedRoute.jsx
@@ -1,16 +1,14 @@
 import { Navigate } from "react-router";
 
 const ProtectedRoute = ({ children }) => {
-  // Por ahora simulamos la autenticación con el localStorage
-  // Tus compañeros del back guardarán aquí el token o el usuario
-  const user = JSON.parse(localStorage.getItem("user-rolling-gym"));
+  // Traemos el usuario del storage para validar el rol
+  const loggedUser = JSON.parse(localStorage.getItem("user-rolling-gym"));
 
-  // Si no hay usuario o no es admin, lo mandamos a la Home
-  if (!user || user.role !== "admin") {
+  // Si no hay usuario o no es admin, lo mandamos a casa
+  if (!loggedUser || loggedUser.rol !== "administrador") {
     return <Navigate to="/" />;
   }
 
-  // Si es admin, lo dejamos pasar al panel
   return children;
 };
 
